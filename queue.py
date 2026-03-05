8e2f6242d38356dcf9e399909e1319e92cc03e03
Otito Amuga <urphasin@gmail.com>
2026-03-04 18:16:24 -0700

    first commit

---
+++ queue.py
@@ -0,0 +1,32 @@
+from collections import deque
+
+class Person:
+    def __init__(self, data):
+        self.name = data
+        self.next = None
+
+class Queue:
+    def __init__(self):
+        self.front = None
+        self.back = None
+
+    def enqueue(self, data: Person):
+        self.back 
+        pass
+
+    
+A = [10, 8, 4, 1, 2, 3, 6, 5, 7]
+
+def twoSums(nums: list, target):
+    for i, n in enumerate(nums):
+        diff = target - n
+        if nums.index(diff):
+            return [i, nums.index(diff)]
+    return -1
+
+print(twoSums(A, 17))
+print("\n\n")
+
+v = twoSums(A, 17)
+print(A[v[0]])
+print(A[v[1]])
