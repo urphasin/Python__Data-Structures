8d999e84e15578844c2e46ff6119c0049537dfe7
Otito Amuga <urphasin@gmail.com>
2026-04-26 22:38:28 -0600

    xcxc

---
+++ algebra/matrix.py
@@ -0,0 +1,16 @@
+import math
+import random
+
+def printMatrix(matrix, m, n):
+    for i in range(m):
+        print("[ ", end="")
+        for j in range(n):
+            print(matrix[i][j], end=" ")
+        print("]")
+        print()
+
+m = 7
+n = 20
+matrix = [[random.randint(0, 1) for _ in range(n)] for _ in range(m)]
+
+printMatrix(matrix=matrix, m=m, n=n)
