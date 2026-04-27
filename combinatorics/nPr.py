98f783553c76d45589bec8ae609dab2d0455db9b
Otito Amuga <urphasin@gmail.com>
2026-04-26 22:40:21 -0600

    sdsd

--- combinatorics/nPr.py
+++ combinatorics/nPr.py
@@ -6,19 +6,6 @@ def nPr(n, r):
         return 0
     return math.factorial(n) / math.factorial(n - r)
 
-def printMatrix(matrix, m, n):
-    for i in range(m):
-        print("[ ", end="")
-        for j in range(n):
-            print(matrix[i][j], end=" ")
-        print("]")
-        print()
-
-m = 7
-n = 20
-matrix = [[random.randint(0, 1) for _ in range(n)] for _ in range(m)]
-
-printMatrix(matrix=matrix, m=m, n=n)
 
 print(nPr(6, 3))
 
