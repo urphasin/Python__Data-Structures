194dbcf52f3dde7f15fdee1d4dde2b642b51e872
Otito Amuga <urphasin@gmail.com>
2026-04-27 07:30:06 -0600

    df

---
+++ combinatorics/nPr.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+long long nPr(int n, int r) {
+    if (r > n || r < 0 || n < 0) {
+        return 0;
+    }
+
+    long long result = 1;
+
+    for (int i = 0; i < r; i++) {
+        result = result * (n - i);
+    }
+
+    return result;
+}
