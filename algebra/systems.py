4f958cee52dd7d14731040b99f9d513df0ddc251
Otito Amuga <urphasin@gmail.com>
2026-04-27 08:37:41 -0600

    xcxc

--- algebra/systems.py
+++ algebra/systems.py
@@ -1,20 +1,45 @@
 import math
+from fractions import Fraction
 
-def det2x2(equation: list[list]):
-    a = equation[0][0]
-    b = equation[0][1]
-    c = equation[1][0]
-    d = equation[1][1]
+def solveByCramersRule(equation: list[list]):
+    def det2x2(equation: list[list]):
+        a = equation[0][0]
+        b = equation[0][1]
+        c = equation[1][0]
+        d = equation[1][1]
+        DET = a*d - b*c
+        if DET == 0:
+            print("Infintely many solutons.")
+        return DET
+    
+    Ax = [
+        [equation[0][2], equation[0][1]],
+        [equation[1][2], equation[1][1]]
+    ]
 
-    DET = a*d - b*c
-    if DET == 0:
-        print("Infintely many solutons.")
-    return DET
+    By = [
+        [equation[0][0], equation[0][2]],
+        [equation[1][0], equation[1][2]]
+    ]
+
+    try:
+        DET = det2x2(equation=equation)
+        detAx = det2x2(Ax)
+        detBy = det2x2(By)
+        solutionX = Fraction(detAx/DET).limit_denominator()
+        solutionY = Fraction(detBy/DET).limit_denominator()
+
+        print(f"X = {solutionX}")
+        print(f"Y = {solutionY}")
+    except ValueError:
+        print("Infinitely many solutions")
 
 
-equation = [
-    [2, 1, 5],
-    [1, -1, 1]
-]
 
-print(det2x2(equation=equation))
+if __name__ == "__main__":
+    equation = [
+        [3, -2, 7],
+        [5, 4, -1]
+    ]
+    print(solveByCramersRule(equation=equation))
+    
