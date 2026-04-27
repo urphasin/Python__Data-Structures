6ebe7192c2504e739d74c9490745cd7f3d5624bd
Otito Amuga <urphasin@gmail.com>
2026-04-27 10:31:07 -0600

    dfdf

---
+++ combinatorics/nCr.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+long long nCr(int n, int r) {
+    if (r > n || r < 0 || n < 0) {
+        return 0;
+    }
+
+
+    long long result = 1;
+    for (int i = 0, j = r; i < r; i++, j--) {
+        result = result * (n - i) / j;
+    }
+
+    return result;
+}
+
+int main() {
+
+    int n = 10, r = 2;
+    printf("nCr = %lld\n", nCr(n, r));
+
+
+    return 0;
+}
